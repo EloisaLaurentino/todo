1f8a5655634dddb210867a6cd63c4cac310e5bf7
LucasSan1 <137550683+LucasSan1@users.noreply.github.com>
2024-01-29 15:06:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const mysql = require('mysql2');
+const express =  require("express");
+const cors = require("cors")
+const app = express();
+
+
+//Midlewares da API
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Coneção com o banco de dados MySQL
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+})
+
+connection.connect(function (err ) {
+    console.log('funfo');
+})
+
+//colocar o call das procedures 
+
+//só um exemplo
+
+    var nome = request.params.id;
+
+connection.query("CALL mudarNome(?)", [nome], function(err,res){
+    if(!err){
+        console.log(res)
+    }
+    else{
+        console.log(err)
+    }
+});

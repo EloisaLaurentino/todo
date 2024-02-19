265a47dbaab458f0793883ea3233ba2a122be6fc
LucasSan1 <137550683+LucasSan1@users.noreply.github.com>
2024-02-19 16:47:28 -0300

    Add files via upload

---
+++ backend/assets/service/service.js
@@ -0,0 +1,84 @@
+const db = require('../database/conn');
+
+    const taskService = {
+   
+
+    taskPost: function(id, nomeTarefa, descricao, categoria){
+        return new Promise((aceito, rejeitado) => {
+            db.query(`insert into TAREFAS (nomeTarefa, descricao, fk_IdLista, tipo) value ('${nomeTarefa}','${descricao}', '${id}', '${tipo}')`, (erro, results) => {
+                if (erro) {rejeitado(erro); return;}
+                aceito('Inserido com Sucesso');
+            });
+        });
+    },
+    taskPut: function(idTarefa, nomeTarefa, descricao){
+        return new Promise((aceito, rejeitado)=>{ 
+            db.query(`UPDATE TAREFAS SET nomeTarefa = '${nomeTarefa}', descricao = '${descricao}' WHERE idTarefa = '${idTarefa}'`, function(error, result){
+                if(error) {rejeitado(error);return;}
+                aceito('Alteração Realizada Com Sucesso!' )
+            });
+        })
+    },
+
+    taskDelete: function(nomeTarefa, ){
+        return new Promise((aceito, rejeitado) =>  {
+            db.query(`DELETE FROM TAREFAS WHERE pk_idTarefa = '${nomeTarefa}'`, (error, result) =>{
+                if(error){rejeitado(error);return;}
+                aceito(result);
+            });
+        });
+    },
+    task: function(id){
+        return new Promise((aceito, rejeitado) => {
+            db.query(`SELECT * FROM TAREFAS`, (erro, results) => {
+                if (erro) {rejeitado(erro); return;}
+                aceito(results);
+            });
+        });
+    },
+    taskId: function(id){
+        return new Promise((aceito, rejeitado) => {
+            db.query(`SELECT * FROM TAREFAS WHERE pk_idTarefa = '${id}'`, (erro, results) => {
+                if (erro) {rejeitado(erro); return;}
+                aceito(results);
+            });
+        });
+    },
+    listPost: function(id, nomeLista){
+        return new Promise((aceito, rejeitado) => {
+            db.query(`insert into LISTAS (nomeLista) value ('${nomeLista}')`, (erro, results) => {
+                if (erro) {rejeitado(erro); return;}
+                aceito('Inserido com Sucesso');
+            });
+        });
+    },
+    listDelete: function(nomeLista, ){
+        return new Promise((aceito, rejeitado) =>  {
+            db.query(`DELETE FROM LISTAS WHERE pk_idLista = '${nomeLista}'`, (error, result) =>{
+                if(error){rejeitado(error);return;}
+                aceito(result);
+            });
+        });
+    },
+
+    list: function(id){
+        return new Promise((aceito, rejeitado) =>  {
+            db.query(`SELECT * FROM LISTAS`, (error, result) =>{
+                if(error){rejeitado(error);return;}
+                aceito(result);
+            });
+        });
+    },
+
+    listId: function(id){
+        return new Promise((aceito, rejeitado) => {
+            db.query(`SELECT * FROM LISTAS WHERE pk_idLista = '${id}'`, (erro, results) => {
+                if (erro) {rejeitado(erro); return;}
+                aceito(results);
+            });
+        });
+
+        }   
+}
+module.exports = taskService;
+
